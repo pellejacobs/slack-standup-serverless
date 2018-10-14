bc3af29c83dce0e5ba1e63e9a25110b14e132702
Pelle Jacobs <pellejacobs@hotmail.com>
2018-10-14 14:36:59 +0200

    Add basic Dynamodb config and setup

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,55 @@
 import { Handler } from 'aws-lambda'
 import { WebClient } from '@slack/client'
+import dynamodb from './dynamodb'
+
+const getMessage = (channel, text) => ({
+  channel,
+  text,
+  attachments: [
+    {
+      text: 'Start or Skip',
+      fallback: 'You are unable to start nor skip',
+      callback_id: 'start_standup',
+      color: '#3AA3E3',
+      actions: [
+        { name: 'standup', text: 'Start', type: 'button', value: 'start' },
+        { name: 'standup', text: 'Skip', type: 'button', value: 'skip' },
+      ],
+    },
+  ],
+})
+
+const createResponse = userId => {
+  const standupDate = new Date().toISOString().substring(0, 10)
+  const params = { TableName: process.env.DYNAMODB_TABLE, Item: { standupDate, userId, status: 'initiated' } }
+  return new Promise((resolve, reject) => {
+    dynamodb.put(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
+}
+
+const sendStandupInit = (users, ims) => userId => {
+  const member = users.find(u => u.id === userId)
+  if (member.name === 'peter') return
+  const imChannel = ims.find(im => im.user === userId)
+  const web = new WebClient(process.env.SLACK_BOT_TOKEN)
+  return Promise.all([
+    web.chat.postMessage(getMessage(imChannel.id, `Hi ${member.real_name}, let's get ready for the standup`)),
+    createResponse(userId),
+  ])
+}
 
 export const handler: Handler = async (event, context, callback) => {
   const web = new WebClient(process.env.SLACK_BOT_TOKEN)
   const usersResponse: any = await web.users.list()
   const imResponse: any = await web.im.list()
-  const users = usersResponse.members
 
   // TODO: Add option for public channels, allow for custom channel
   const groupsResponse: any = await web.groups.list()
   const channel = groupsResponse.groups.find(g => g.name === 'standup-test')
 
-  await Promise.all(
-    channel.members.map(async code => {
-      const member = users.find(u => u.id === code)
-      if (member.name === 'peter') return
-      const imChannel = imResponse.ims.find(im => im.user === code)
-      const message = `Hi ${member.real_name}, let's get ready for the standup`
-      await web.chat.postMessage({
-        channel: imChannel.id,
-        text: message,
-        attachments: [
-          {
-            text: 'Start or Skip',
-            fallback: 'You are unable to start nor skip',
-            callback_id: 'start_standup',
-            color: '#3AA3E3',
-            actions: [
-              { name: 'standup', text: 'Start', type: 'button', value: 'start' },
-              { name: 'standup', text: 'Skip', type: 'button', value: 'skip' },
-            ],
-          },
-        ],
-      })
-    })
-  )
-
+  await Promise.all(channel.members.map(sendStandupInit(usersResponse.members, imResponse.ims)))
   callback(null, {})
 }
